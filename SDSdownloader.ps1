fac5111d11fb24613e5b9342494e0a2b432d4107
Saulotas <Saulotas@users.noreply.github.com>
2020-09-30 17:32:26 +0300

    Update SDSdownloader.ps1
    Added a check for both of url types

--- SDSdownloader.ps1
+++ SDSdownloader.ps1
@@ -16,12 +16,24 @@ New-Item -path "$sourcepath\PDF" -ItemType "directory"
 
 $workpath = $sourcepath +'\PDF\'
 
+
+
 foreach  ($line in  [System.IO.File]::ReadLines($sourcefile)| Where-Object { $_.Trim() -ne ''})
 {
+    if ($line -contains ("https"))
+        {
             $filename = $workpath+$line.Substring($line.LastIndexOf('=')+1)+'.pdf'
             [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
             #Invoke-WebRequest 
             Invoke-RestMethod $line -OutFile $filename
+        }
+    else
+        {
+            $filename = $workpath+$line.Substring($line.LastIndexOf('%3D')+3)+'.pdf'
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            #Invoke-WebRequest 
+            Invoke-RestMethod $line -OutFile $filename
+        }
 }
 
 Invoke-item $workpath
